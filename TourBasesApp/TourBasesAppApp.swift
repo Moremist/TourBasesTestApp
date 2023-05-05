efbc12769fb5e3690e8a0cabd7c658f7083757bb
Roman Kuzmich <rbest15@rambler.ru>
2023-05-05 16:36:18 +0300

    Initial Commit

---
+++ TourBasesApp/TourBasesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TourBasesAppApp.swift
+//  TourBasesApp
+//
+//  Created by Roman Kuzmich on 05.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TourBasesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
